0ff4ad148aa2e85a6f40c689612ad546e6fead64
Teri Pastorino <tpasto@comcast.net>
2015-03-11 18:50:59 -0700

    wip my mess

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
   describe "signup page" do
     it "should have the content 'Sign up'" do
       visit new_user_registration_path
+      expect(page).to have_content('Sign up')
     end
   end
   
@@ -22,20 +23,21 @@ require 'rails_helper'
   end
 
   describe "authenticate" do
-         visit new_user_registration_path
-
-    it "should validate email" do
-
+  #  before do 
+      visit new_user_registration_path
+    
         fill_in 'Username', with: 'username'
-        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Email', with: ' '
         fill_in 'Password', with: 'password'
         fill_in 'Password confirmation', with: 'password'
         click_button 'Sign up'
+    
+      expect('Email').not_to eq(" ")
+  end
+      
+    
 
-        expect_error_messages "Email is invalid"
-      end
-
-
+    
 
       describe "confirm user" do
         it "receives email confirmation" do
@@ -44,22 +46,10 @@ require 'rails_helper'
         end
       end
 
-
-
-     
-
-
-    describe 'User can sign out' do
-      it 'as a user' do
+    describe "User can sign out" do
+      it "as a user" do
         #sign_in
-
         #click_link "Logout"
-
         #expect(page).to have_content "Sign up"
       end
     end
-  
-
-#
-
-
