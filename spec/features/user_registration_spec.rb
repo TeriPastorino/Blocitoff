d27a681db2b2859fe97f2fe5e7df02f8390746b2
Teri Pastorino <tpasto@comcast.net>
2015-03-13 15:46:21 -0700

    wip signing in tests

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,81 +1,83 @@
 require 'rails_helper'
 
-  describe "signup page" do
+#include TestFactories
+
+def signup(username, email, password)
+  visit new_user_registration_path
+  fill_in 'Username', with: username
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password
+  click_button 'Sign up'
+end
+
+describe "User registration" do
+  describe "Without validation errors" do
     it "should have the content 'Sign up'" do
       visit new_user_registration_path
       expect(page).to have_content('Sign up')
     end
   end
+
+  it "redirects to the index page" do
+    signup('newuser', 'newuser@example.com', 'password')
+    expect(current_path).to eq "/"
+    expect(page).to have_content('activate')
+  end  
+end
+
+
+describe "confirm user" do
+  it "sends a confirmation email" do
+    let(:current_email) {ActionMailer::Base.deliveries.last}
+    # This is what let does for us
+    # def current_email
+    #   # memoize the response
+    #   @current_email ||= ActionMariler::Base.delveries.last
+    # end
+    signup('newuser', 'newuser@example.com', 'password')
+    expect(page).to have_content("activate your account")
+    expect(current_email.to.first).to eq('newuser@example.com')
+  end
+end
   
-# THIS TEST IF FAILING - it says users already exists - set up user factory? and use database cleaner to test???
-  describe "sign up user" do
-    it "new account" do
-      visit new_user_registration_path
-        fill_in 'Username', with: 'newuser'
-        fill_in 'Email', with: 'newuser@example.com'
-        fill_in 'Password', with: 'password'
-        fill_in 'Password confirmation', with: 'password'
-        click_button 'Sign up'
-
-        expect(current_path).to eq "/users"
-        expect(page).to have_content('activate')
-    end
-  end
-
-  #This is not testing correctly because it should stop due to missing email address
-  describe "authenticate" do
-    before do 
-      visit new_user_registration_path
-    end
-      it "is invalid without an email" do
-    
-        fill_in 'Username', with: 'newtestuser'
-        fill_in 'Email', with: (" ")
-        fill_in 'Password', with: 'password'
-        fill_in 'Password confirmation', with: 'password'
-        click_button 'Sign up'
-    
-      expect('Email').to not_to eq(" ")
-      expect(flash[:error]).to eq "Invalid Email"
+describe "without filling in an email" do
+  it "displays 'Email can't be blank'" do
+    signup('newuser', '', 'password')
+    expect(page).to have_content("Email can't be blank")
   end
 end
-      
-    
 
-    #is this supposed to be a controller test? how do i know which tests go where, can we make a list
 
-      describe "confirm user" do
-        it "receives email confirmation" do
-        expect(page).to have_content("Email sent")
-        ActionMailer::Base.deliveries.last
-        #expect(last_email).to include(user.email)
-        #example from railscasts - don't have these methods
-
-        end
-      end
-# do i need to set up factory stuff to get this to work?
-      describe "unique email" do
-      xit "signs up new user" do
-      end
-    end
-
-    #the next two go together but how to set up?
-
-    describe "display a user" do
-      xit "is signed in" do
-      before do
-        #user signed in
-
+describe "using an already existing email" do
+  it "displays 'Email is already taken'" do
+    #before do
+    create(:user, email: "existing@example.com")
+  
+    signup('newuser','existin@example.com','password')
+    expect(page).to have_content("that email is already taken")
+  end
 end
 
-      end
-    end
+def signin(username, email, password)
+  visit user_session_path
+  fill_in 'Username', with: username
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password
+  click_button 'Sign in'
+end
+# As a user, I want to sign in and out of Blocitoff.
+ 
+describe "Signing in" do
+  describe "successfully" do
+    signin('newuser','existin@example.com','password')
 
-
-    describe "User can sign out" do
-      xit "as a user" do
-        #sign_in
-        #click_link "Logout"
-        #expect(page).to have_content "Sign up"
-      end
-    end
+    it "displays a flash message indicating success"
+    expect(page).to have_content("Signed in successfully")
+  end
+end
+ 
+  describe "unsuccsfully" do
+    it "re-renders the page"
+  end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,8 +19,9 @@ require 'rspec/rails'
 # of increasing the boot-up time by auto-requiring all files in the support
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
-#
-# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+#uncomment this line for database cleaner
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
