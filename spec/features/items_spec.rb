44190383bd79bfef1689a5188ee03257353814d8
Teri Pastorino <tpasto@comcast.net>
2015-03-26 10:16:04 -0700

    WIP testing items

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -1,26 +1,33 @@
 require 'rails_helper'
 
 describe "todo items" do
-  include TestFactories
-  include Warden::Test::Helpers
-  Warden.test_mode!
+  before { login_as FactoryGirl.create(:user) } do
 
-  #need a signed in user
-
-describe "successful" do
-  it "can create multiple"
+describe "successfully" do
+  it "can display" do
     visit user_session_path
-    fill_in "Description", with: "Really long text..."
+    fill_in "Description", with: "Really long text"
     click_button('Add Item To List')
-    
+    expect(page). to have_content "Really long text"
   end
-
-  it "displays new"
+end
+#? can you loop x num of times
+xit "can create multiple" do
+  visit user_session_path
+    fill_in "Description", with: "Really long text"
+    click_button('Add Item To List')
   end
-
-   it "can delete multiple"
+#is click_link proper for selecting the glyph and how do i tell it which one?
+   xit "can delete multiple" do
+    visit user_session_path
+    click_link 'glyphicon glyphicon-ok'
+    expect
   end
-  00p
-   it "use ajax to delete"
+#how to distinguish in test delete vs delete w/ajax?
+   xit "use ajax to delete" do
+    visit user_session_path
+    click_link 'glyphicon glyphicon-ok'
+    expects
   end
+end
 end
